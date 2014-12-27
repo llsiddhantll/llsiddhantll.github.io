f9b5c51b50e204704a22768ec30595da28316d04
Siddhant <llsiddhantll@gmail.com>
2014-12-28 01:09:38 +0530

    Housie v2.0

--- js/site.js
+++ js/site.js
@@ -1,38 +1,61 @@
 $(document).ready(function(){
 
-  var str = "<div id='num1' class='number col-xs-1 col-xs-offset-1'><h1>1</h1></div>",
-      count = 2,
-      str2 = "<div class='row'><paper-button class='mainBtn generateBtn col-xs-2 col-xs-offset-5'><h3>Generate</h3></paper-button></div>";
+  var tabs = document.querySelector('paper-tabs'),
+      pages = document.querySelector('core-pages'),
+      repositionCircle = function(){
+        var leftPos = $(window).width()/2 - 150;
+        $('.selectedNumber').css("left",leftPos+"px");
+      },
+      repositionGenerate = function(){
+        var leftPos = $(window).width()/2 - 75;
+        $('#generateBtn').css("left",leftPos+"px");
+      },
+      str="";
 
-  for(var i=0 ; i<99 ; i++){
-    str += "<div id='num" + count + "' class='number col-xs-1'><h1>" + count + "</h1></div>";
-    count++;
+  tabs.addEventListener('core-select',function(){
+    pages.selected = tabs.selected;
+  });
+
+  $('#generateBtn').click(function(){
+    var newNum = parseInt(Math.random()*100),
+        numArray = [];
+
+    for(var i=0 ; i<99 ; i++)
+    {
+      numArray[i] = 0;
+    }
+
+    while(numArray[newNum] === 1)
+    {
+      newNum = parseInt(Math.random()*100);
+    }
+    numArray[newNum] = 1;
+
+    $('.selectedNumber').html("<p>"+ newNum +"</p>");
+    $('.currentNumber').html("<h3>Current:</h3><br>" + newNum);
+    $('#num'+newNum).css('background-image',"url('/images/used.svg')");
+  });
+
+  $(window).resize(function(){
+    repositionCircle();
+    repositionGenerate();
+  });
+
+  repositionCircle();
+  repositionGenerate();
+
+  //GENERATE NUMBER DIVS INSIDE CONTAINER
+
+  for(var i=1 ; i<=10 ; i++)
+  {
+    str+="<div class='row'><div id='num" + ((i-1)*10+1) + "' class='numbers col-xs-1 col-xs-offset-2'><p>" + ((i-1)*10+1) + "</p></div>";
+    for(var j=2 ; j<=10 ; j++)
+    {
+      str+="<div id='num" + ((i-1)*10+j) + "' class='numbers col-xs-1'><p>" + ((i-1)*10+j) + "</p></div>";
+    }
+    str+="</div>";
   }
-
-  str += str2;
 
   $('.container').html(str);
 
-  $('.generateBtn').click(function(){
-    var nums = [],
-    newNum = parseInt(Math.random()*100);
-
-    for(var i=0 ; i<99 ; i++){
-      nums[i] = 0;
-    }
-
-    while(nums[newNum-1] === 1){
-      newNum = parseInt(Math.random()*100);
-    }
-
-    nums[newNum-1] = 1;
-    var select = "#num" + newNum;
-    $(select).css("background-image","url('/images/used.svg')");
-
-    str2 = "";
-    $('#dialogContent').html("<h1> " + newNum + "</h1>");
-    document.querySelector('#dialog').toggle();
-    //alert("Lucky Number " + newNum);
-    console.log(newNum);
-  });
 });
