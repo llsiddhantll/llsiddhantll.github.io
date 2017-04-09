0252c70b10e6c18b581602fcd570dcc7245323c3
Siddhant <me@madebysid.com>
2017-04-10 01:56:00 +0530

    Finish up About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import IconLink from './IconLink';
 
 export default class About extends Component {
   render () {
     return (
       <div className='about'>
-        About
+        <div className='about-content'>
+          <div className='purple-shape-1' />
+
+          <div className='about-text'>
+            <div className='about-hero'>
+              <div className='code about-greeting'>Hola!</div>
+              <div className='code about-name'>I'm Siddhant, <br /> and I do things with Javascript</div>
+              <div className='code about-subtext'>(and a little bit of illustrator)</div>
+            </div>
+
+            <div className='regular about-description'>
+              I’m a 22-year old graduate who loves everything web. 
+              I’ve been working with it for close to four years now.
+              I love learning about new ideas and the tech they inspire.<br />
+              I’m obsessed with Rocket League and keeping my Downloads folder organized. 
+            </div>
+          </div>
+
+          <div className='green-shape-1' />
+        </div>
+
+        <div className='about-footer'>
+          <div className='about-resume'>
+            <a href='../../resume.pdf' target='_blank'>Download my Resume</a>
+          </div>
+
+          <div className='about-links'>
+          {
+            _.map(this.props.links, (link) => {
+              return (
+                <IconLink
+                  icon={link.icon}
+                  key={link.href}
+                  onClick={this.props.onLinkClick.bind(this, link)}
+                />
+              );
+            })
+          }
+          </div>
+        </div>
       </div>
-    )
+    );
   }
+};
+
+About.propTypes = {
+  links: PropTypes.array
 }
--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -5,14 +5,47 @@ import About from '../components/About';
 
 import '../css/about.css';
 
+let links = [{
+  icon: 'linkedin',
+  href: 'https://linkedin.com/in/madebysid'
+}, {
+  icon: 'github',
+  href: 'https://github.com/madebysid'
+}, {
+  icon: 'dribbble',
+  href: 'https://dribbble.net/madebysid'
+}, {
+  icon: 'behance',
+  href: 'https://behance.net/in/madebysid'
+}, {
+  icon: 'twitter',
+  href: 'https://twitter.com/llsiddhantll'
+}, {
+  icon: 'angellist',
+  href: 'https://angel.co/siddhantsinha'
+}]
+
 export default class AboutContainer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleLinkClick = this.handleLinkClick.bind(this)
+  }
+
+  handleLinkClick (link) {
+    link.href && window.open(link.href);
+  }
+
   render () {
     return (
       <div className='about-container'>
         <HeaderContainer
           selected='about'
         />
-        <About />
+        <About
+          links={links}
+          onLinkClick={this.handleLinkClick}
+        />
       </div>
     );
   }
