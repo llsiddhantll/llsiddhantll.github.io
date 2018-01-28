7a45ec5453224f06adc46ac41c01a6e771c01f18
Siddhant <me@madebysid.com>
2018-01-28 18:21:24 +0530

    Make hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import HeaderLinks from './HeaderLinks';
+import HamburgerMenu from './HamburgerMenu';
 
 import '../css/header.css';
 
@@ -9,6 +10,9 @@ export default class Header extends Component {
   render () {
     return (
       <div className='header'>
+        <HamburgerMenu
+          links={this.props.links}
+        />
         <a className='logo' href='#home'>
           <div className='logo__image' />
         </a>
