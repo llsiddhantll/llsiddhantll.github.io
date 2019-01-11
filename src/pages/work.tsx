35d51e36daa41ddbd7db3a11586faca63ccd2728
Siddhant <me@madebysid.com>
2019-01-11 12:50:55 +0100

    Switch to Gatsby

---
+++ src/pages/work.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <div>Work</div>
+  </Layout>
+)
