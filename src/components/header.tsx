8764423e7c33a32fa2b42c69d2493c2a7eda26cc
Siddhant <me@madebysid.com>
2019-01-17 15:04:49 +0100

    Add a Resume link to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,6 +8,8 @@ type HeaderLink = {
   id: string
   label: string
   url: string
+  external?: boolean
+  highlight?: true
 }
 
 const links: HeaderLink[] = [{
@@ -22,6 +24,12 @@ const links: HeaderLink[] = [{
   id: 'projects',
   label: 'Projects',
   url: '#projects'
+}, {
+  id: 'resume',
+  label: 'Resume',
+  url: 'https://goo.gl/P6eL5k',
+  external: true,
+  highlight: true
 }];
 
 const Header: React.SFC<{}> = () => (
@@ -32,16 +40,32 @@ const Header: React.SFC<{}> = () => (
       from={{ transform: 'translateY(-40px)' }}
       to={{ transform: 'translateY(0px)' }}
     >
-      {(link: HeaderLink) => props => (
-        <Link
-          style={props}
-          className={styles.link}
-          activeClassName={styles.active}
-          to={link.url}
-        >
-          {link.label}
-        </Link>
-      )}
+      {(link: HeaderLink) => props => {
+        if (link.external) {
+          return (
+            <a
+              style={props}
+              className={styles.linkExternal}
+              href={link.url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {link.label}
+            </a>
+          ) 
+        }
+
+        return (
+          <Link
+            style={props}
+            className={styles.link}
+            activeClassName={styles.active}
+            to={link.url}
+          >
+            {link.label}
+          </Link>
+        ) 
+      }}
     </Trail>
   </div>
 )
