11e4ea2b42694330bb62fb6ab93485665bbf7690
Siddhant <me@madebysid.com>
2019-01-22 00:47:02 +0100

    Projects section

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import data from "../data/projects";
+
 import Section from "../components/section";
+import Paragraph from "../components/Paragraph";
 import styles from '../styles/projects.module.css';
 
 const Projects: React.SFC<{}> = () => (
-  <Section id="projects" title="Projects">
-    Projects
+  <Section id="projects" title={data.title}>
+    <Paragraph text={data.text} />
   </Section>
 );
 
