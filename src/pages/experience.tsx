efb21980ab0e261edf1d6e28def24a9fd82b5a27
Siddhant <me@madebysid.com>
2019-01-22 00:46:45 +0100

    Experience section

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import data from "../data/experience.json";
+
 import Section from "../components/section";
+import Paragraph from "../components/paragraph";
+import List from "../components/list";
+
 import styles from '../styles/experience.module.css';
 
 const Experience: React.SFC<{}> = () => (
-  <Section id="experience" title="Experience">
-    Experience
+  <Section id="experience" title={data.title}>
+    {data.companies.map((company, idx) => (
+      <div className={styles.company}>
+        <div className={styles.header}>
+          <div className={styles.title}>{company.title}</div>
+          <div className={styles.years}>({company.years})</div>
+          <a
+            href={company.website.link} rel="noopener noreferrer"
+            className={styles.website}
+          >
+            {company.website.label}
+          </a>
+        </div>
+        <Paragraph text={company.text} />
+        <List items={company.tech} />
+      </div>
+    ))}
   </Section>
 );
 
