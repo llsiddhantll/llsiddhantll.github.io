769f3e746f9c98f434c42bbf69685e25b4cff203
Siddhant <me@madebysid.com>
2019-01-23 09:20:28 +0100

    Add in remark to allow paragraphs to include Markdown

--- src/components/paragraph.tsx
+++ src/components/paragraph.tsx
@@ -1,20 +1,26 @@
 import React from "react";
+import remark from 'remark';
+import remark2react from 'remark-react';
+import links from 'remark-external-links';
 
 import styles from "../styles/paragraph.module.css";
 
 interface Props {
   text: string[],
   size?: number,
-  className?: CSSProperties
+  className?: React.CSSProperties
 }
 
 const Paragraph: React.SFC<Props> = ({ text, className }) => (
   <div className={[styles.container, className].join(' ')}>
     {text.map((line: string, idx: number) => (
       <React.Fragment key={idx}>
-        {line}
-        <br />
-        <br />
+        {
+          remark()
+            .use(links)
+            .use(remark2react)
+            .processSync(line).contents
+        }
       </React.Fragment>
     ))}
   </div>
