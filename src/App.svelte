5f28a9e3198386dc4c47be2d5e4110c73d0293dd
Siddhant <me@madebysid.com>
2019-08-18 17:45:22 +0200

    Oe more commit to check aliasing

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,1 @@
-<script>
-  export let name;
-</script>
-
-<style>
-  h1 {
-    color: purple;
-  }
-</style>
-
-<h1>Hello {name}!</h1>
+<h1>Svelte website</h1>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import App from "./App.svelte";
+import App from './App.svelte'
 
 const app = new App({
   target: document.body,
-  props: {
-    name: "world"
-  }
-});
+  props: {},
+})
 
 // window.app = app;
 
-export default app;
+export default app
