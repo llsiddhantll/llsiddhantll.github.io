72761c4532c5b70bffde37556a397e4bc813860c
Siddhant <me@madebysid.com>
2019-08-26 13:44:15 +0200

    Add an About page

---
+++ src/pages/About.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Page from 'components/Page.svelte'
+  import Paragraph from 'components/Paragraph.svelte'
+  import List from 'components/List.svelte'
+
+  import data from 'data/about.json'
+</script>
+
+<Page id="about" title="About">
+  <Paragraph>
+    {#each data.text as paragraph}
+      <p>{paragraph}</p>
+    {/each}
+  </Paragraph>
+
+  <List columns={2} items={data.tech} />
+</Page>
