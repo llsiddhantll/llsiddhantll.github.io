3013aaa1417671dfbe575401d40787f08cf5270b
Siddhant <me@madebysid.com>
2019-08-26 13:44:42 +0200

    Add dummy pages for contact, home and projects

---
+++ src/pages/Contact.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Page from 'components/Page.svelte'
+</script>
+
+<Page id="contact" title="Contact">
+  <h1>Contact</h1>
+</Page>
---
+++ src/pages/Projects.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Page from 'components/Page.svelte'
+</script>
+
+<Page id="projects" title="Projects">
+  <h1>Projects</h1>
+</Page>
