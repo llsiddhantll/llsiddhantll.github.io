19929dbc42423f0ba3e2162be6e45370b406ee21
Siddhant <me@madebysid.com>
2019-12-31 22:24:54 +0100

    Add base components

---
+++ src/components/base/Paragraph.svelte
@@ -0,0 +1,15 @@
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+  }
+
+  .container :global(a) {
+    color: #00b7ff;
+  }
+</style>
+
+<div class="container">
+  <slot />
+</div>
