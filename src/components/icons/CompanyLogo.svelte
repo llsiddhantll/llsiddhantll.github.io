79bff900fa52ed292b5c817cfdd0ac2541bd1e37
Siddhant <me@madebysid.com>
2019-12-31 22:24:40 +0100

    Add assets

---
+++ src/components/icons/CompanyLogo.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Prisma from './Prisma.svelte'
+  import Perdoo from './Perdoo.svelte'
+  import Postman from './Postman.svelte'
+
+  export let name
+</script>
+
+<style>
+  .logo :global(svg) {
+    height: 30px;
+    max-width: 30px;
+  }
+</style>
+
+<div class="logo">
+  {#if name === 'prisma'}
+    <Prisma />
+  {:else if name === 'perdoo'}
+    <Perdoo />
+  {:else if name === 'postman'}
+    <Postman />
+  {/if}
+</div>
