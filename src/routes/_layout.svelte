e5df9e8122abfd0be0a1256b7201c1f3a9b41c15
Siddhant <me@madebysid.com>
2019-12-31 22:23:29 +0100

    Set up layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,27 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-
-	export let segment;
+  import Header from '../components/Header.svelte'
+  import Footer from '../components/Footer.svelte'
+  import Home from '../components/sections/Home.svelte'
+  import About from '../components/sections/About.svelte'
+  import Experience from '../components/sections/Experience.svelte'
+  import Projects from '../components/sections/Projects.svelte'
+  import Contact from '../components/sections/Contact.svelte'
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    display: grid;
+    grid-template-columns: 1fr 960px 1fr;
+    grid-template-rows: 50px 1fr;
+  }
 </style>
 
-<Nav {segment}/>
-
 <main>
-	<slot></slot>
+  <Header />
+  <Home />
+  <About />
+  <Experience />
+  <Projects />
+  <Contact />
+  <Footer />
 </main>
