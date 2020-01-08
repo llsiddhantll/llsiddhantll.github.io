8f928345e85aa1ad647b0881167a6ca7d4c436ff
Siddhant <me@madebysid.com>
2020-01-08 10:04:15 +0100

    Hardcode contact

--- src/components/sections/Contact.svelte
+++ src/components/sections/Contact.svelte
@@ -2,8 +2,6 @@
   import Section from '../base/Section.svelte'
   import Paragraph from '../base/Paragraph.svelte'
   import List from '../base/List.svelte'
-
-  import data from '../../data/contact.json'
 </script>
 
 <style>
@@ -15,13 +13,19 @@
 <Section id="contact" title="Contact">
   <div class="opinions">
     <Paragraph>
-      {#each data.text as line}
-        <p>
-          {@html line}
-        </p>
-      {/each}
+      <p>
+        If you're convinced, and want to have a chat with me, hit me up! You can
+        reach me at
+        <a href="mailto:me@madebysid.com?subject=Howdy!">me @ madebysid.com</a>
+        . If you're in Berlin and want to hang, I'm open!"
+      </p>
+
+      <p>Check out my presence on the interwebs!</p>
+
+      <p>Let's end with some opinions no one asked for:</p>
     </Paragraph>
   </div>
 
-  <List items={data.opinions} />
+  <List
+    items={['VS Code > Sublime Text', 'Sublime Text > VS Code (in my heart)', 'Sublime Merge > git CLI', 'TypeScript > JavaScript', 'CSS Modules > CSS-in-JS (bring on the pitchforks)']} />
 </Section>
