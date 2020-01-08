e4ff1956185fb1374f115164a4e1fb3619724d6b
Siddhant <me@madebysid.com>
2020-01-08 12:45:16 +0100

    Set up ELB deploy script for Actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
 PORT=8080 npm run build
-eb deploy
+
+# Set up useful variables
+commit=$(git rev-parse --short HEAD)
+version=${commit}
+archive=website-$commit.zip
+elbApp=website
+elbEnvironment=website-dev
+s3bucket=siddhant-website-elb-bundles
+
+# Create the code archive to upload to ELB, deleting old ones, if they exist
+rm -rf $archive
+zip \
+  -x ".git/**" ".github/**" "node_modules/**" "__sapper__/dev/**" \
+  -r $archive .
+
+# Copy the archive to S3
+aws s3 cp $archive s3://$s3bucket
+
+# Create a new version on ELB
+aws elasticbeanstalk create-application-version \
+  --application-name $elbApp \
+  --version-label $version \
+  --source-bundle S3Bucket=$s3bucket,S3Key=$archive
+
+# Update the environment to use this new version
+aws elasticbeanstalk update-environment \
+  --application-name $elbApp \
+  --environment-name $elbEnvironment \
+  --version-label $version
+
+# Delete the code archive, since it now resides on S3
+rm -rf $archive
