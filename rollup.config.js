88fa8b9ba9dd7bbc7388b15dba8ac0095d3566a4
Siddhant <me@madebysid.com>
2020-05-05 14:18:25 +0200

    Bump up dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,22 @@
-import resolve from '@rollup/plugin-node-resolve'
-import replace from '@rollup/plugin-replace'
-import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
-import svelte from 'rollup-plugin-svelte'
-import babel from 'rollup-plugin-babel'
-import { terser } from 'rollup-plugin-terser'
-import config from 'sapper/config/rollup.js'
-import pkg from './package.json'
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import svelte from "rollup-plugin-svelte";
+import { terser } from "rollup-plugin-terser";
+import config from "sapper/config/rollup.js";
+import pkg from "./package.json";
 
-const mode = process.env.NODE_ENV
-const dev = mode === 'development'
-const legacy = !!process.env.SAPPER_LEGACY_BUILD
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-  (warning.code === 'CIRCULAR_DEPENDENCY' &&
+  (warning.code === "CIRCULAR_DEPENDENCY" &&
     /[/\\]@sapper[/\\]/.test(warning.message)) ||
-  onwarn(warning)
-const dedupe = importee =>
-  importee === 'svelte' || importee.startsWith('svelte/')
+  onwarn(warning);
+const dedupe = (importee) =>
+  importee === "svelte" || importee.startsWith("svelte/");
 
 export default {
   client: {
@@ -25,52 +24,28 @@ export default {
     output: config.client.output(),
     plugins: [
       replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode)
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
       }),
       svelte({
         dev,
         hydratable: true,
-        emitCss: true
+        emitCss: true,
       }),
       resolve({
         browser: true,
-        dedupe
+        dedupe,
       }),
       commonjs(),
       json(),
 
-      legacy &&
-        babel({
-          extensions: ['.js', '.mjs', '.html', '.svelte'],
-          runtimeHelpers: true,
-          exclude: ['node_modules/@babel/**'],
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: '> 0.25%, not dead'
-              }
-            ]
-          ],
-          plugins: [
-            '@babel/plugin-syntax-dynamic-import',
-            [
-              '@babel/plugin-transform-runtime',
-              {
-                useESModules: true
-              }
-            ]
-          ]
-        }),
-
       !dev &&
         terser({
-          module: true
-        })
+          module: true,
+        }),
     ],
 
-    onwarn
+    onwarn,
   },
 
   server: {
@@ -78,24 +53,24 @@ export default {
     output: config.server.output(),
     plugins: [
       replace({
-        'process.browser': false,
-        'process.env.NODE_ENV': JSON.stringify(mode)
+        "process.browser": false,
+        "process.env.NODE_ENV": JSON.stringify(mode),
       }),
       svelte({
-        generate: 'ssr',
-        dev
+        generate: "ssr",
+        dev,
       }),
       resolve({
-        dedupe
+        dedupe,
       }),
       commonjs(),
-      json()
+      json(),
     ],
     external: Object.keys(pkg.dependencies).concat(
-      require('module').builtinModules ||
-        Object.keys(process.binding('natives'))
+      require("module").builtinModules ||
+        Object.keys(process.binding("natives"))
     ),
 
-    onwarn
-  }
-}
+    onwarn,
+  },
+};
